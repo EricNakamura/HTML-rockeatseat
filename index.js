66119cf3de23ad79c395f36e7acbd99c79b33907
EricNakamura <masaro.eric@gmail.com>
2023-08-28 04:57:31 +0100

    fixes

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function toggleTheme() {
   html.classList.toggle("light");
   if (html.classList.contains("light")) {
     avatar.setAttribute("src", "./assets/avatar-light.png");
+    avatar.setAttribute("alt", "Uma foto da lenda do CSS Mayk Brito com óculos de Sol")
   } else {
     avatar.setAttribute("src", "./assets/avatar.png");
+    avatar.setAttribute("alt", "Uma foto da lenda do CSS Mayk Brito")
   }
 }
